c2a28503d2b5f20ef8f0df29f8be464a15de07a6
mango <darkdon66@naver.com>
2023-09-04 03:27:14 +0900

    NewBranch

---
+++ src/components/SelectedBlocks.tsx
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+import { Block } from "./CustomBlocks";
+import { registerTemplate } from "../lib/api";
+import { Dispatch, SetStateAction } from "react";
+interface SelectedBlockProps extends React.ComponentPropsWithRef<"div"> {
+  selectedBlocks: Block[];
+  formDataSet: {
+    defaultBlock: any[];
+    personalinfoBlock: any[];
+    safetyBlock: any[];
+    responsibilityBlock: any[];
+    paymentBlock: any[];
+    etcBlock: any[];
+  };
+  //   setFormDataSet: {
+  //     defaultBlock: any[];
+  //     personalinfoBlock: any[];
+  //     safetyBlock: any[];
+  //     responsibilityBlock: any[];
+  //     paymentBlock: any[];
+  //     etcBlock: any[];
+  //   };
+}
+
+export default function SelectedBlockList(props: SelectedBlockProps) {
+  let safetyBlock: any = [];
+  let personalInfoBlock: any = [];
+  let responsibilityBlock: any = [];
+  let paymentBlock: any = [];
+  let defaultBlock: any = [];
+  let etcBlock: any = [];
+
+  const [formDataSet, setFormDataSet] = useState({
+    defaultBlock: [],
+    personalinfoBlock: [],
+    safetyBlock: [],
+    responsibilityBlock: [],
+    paymentBlock: [],
+    etcBlock: [],
+  });
+
+  for (let i = 0; i < props.selectedBlocks.length; i++) {
+    if (props.selectedBlocks[i].category === "safety") {
+      safetyBlock.push(props.selectedBlocks[i].content);
+      setFormDataSet({ ...formDataSet, safetyBlock: safetyBlock });
+    } else if (props.selectedBlocks[i].category === "personal-info") {
+      personalInfoBlock.push(props.selectedBlocks[i].content);
+      setFormDataSet({ ...formDataSet, personalinfoBlock: personalInfoBlock });
+    } else if (props.selectedBlocks[i].category === "responsibility") {
+      responsibilityBlock.push(props.selectedBlocks[i].content);
+      setFormDataSet({
+        ...formDataSet,
+        responsibilityBlock: responsibilityBlock,
+      });
+    } else if (props.selectedBlocks[i].category === "payment") {
+      paymentBlock.push(props.selectedBlocks[i].content);
+      setFormDataSet({ ...formDataSet, paymentBlock: paymentBlock });
+    } else if (props.selectedBlocks[i].category === "default") {
+      defaultBlock.push(props.selectedBlocks[i].question);
+      setFormDataSet({ ...formDataSet, defaultBlock: defaultBlock });
+    } else {
+      etcBlock.push(props.selectedBlocks[i].content);
+      setFormDataSet({ ...formDataSet, etcBlock: etcBlock });
+    }
+  }
+  safetyBlock = safetyBlock.join("<br/>");
+  personalInfoBlock = personalInfoBlock.join("<br/>");
+  responsibilityBlock = responsibilityBlock.join("<br/>");
+  paymentBlock = paymentBlock.join("<br/>");
+  defaultBlock = defaultBlock.join("<br/>");
+  etcBlock = etcBlock.join("<br/>");
+
+  setFormDataSet({
+    ...formDataSet,
+    defaultBlock: defaultBlock,
+    personalinfoBlock: personalInfoBlock,
+    safetyBlock: safetyBlock,
+    responsibilityBlock: responsibilityBlock,
+    paymentBlock: paymentBlock,
+    etcBlock: etcBlock,
+  });
+
+  const handleRegisterTemplate = () => {
+    if (formDataSet !== null) {
+      registerTemplate(formDataSet)
+        .then((res) => {
+          console.log(res.data);
+        })
+        .catch((err) => {
+          console.error("Error:", err.response.data.message);
+        });
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <h3 style={{ fontSize: "20px", fontWeight: "bold" }}>Default</h3>
+        <p dangerouslySetInnerHTML={{ __html: defaultBlock }}></p>
+        <h3 style={{ fontSize: "20px", fontWeight: "bold" }}>Personal-info</h3>
+        <p dangerouslySetInnerHTML={{ __html: personalInfoBlock }}></p>
+        <h3 style={{ fontSize: "20px", fontWeight: "bold" }}>Safety</h3>
+        <p dangerouslySetInnerHTML={{ __html: safetyBlock }}></p>
+        <h3 style={{ fontSize: "20px", fontWeight: "bold" }}>Responsibility</h3>
+        <p dangerouslySetInnerHTML={{ __html: responsibilityBlock }}></p>
+        <h3 style={{ fontSize: "20px", fontWeight: "bold" }}>Payment</h3>
+        <p dangerouslySetInnerHTML={{ __html: paymentBlock }}></p>
+        <h3 style={{ fontSize: "20px", fontWeight: "bold" }}>Etc</h3>
+        <p dangerouslySetInnerHTML={{ __html: etcBlock }}></p>
+      </div>
+    </div>
+  );
+}
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -53,6 +53,11 @@ export const signUp = async ({
     throw error; // 호출자에게 예외 다시 던지기
   }
 };
+
+// const postData = defaultBlocks.map((blocks, index) => {
+
+//   return obj;
+// });
 export const identify = async ({
   userId,
   pw,
@@ -84,8 +89,47 @@ export const identify = async ({
   }
 };
 
-export const registerTemplate = (selectedBlocks: Block[]) =>
-  api.post("/template/make", selectedBlocks);
+// export const registerTemplate = ({
+//   default,
+//   personalinfo,
+//   safety,
+//   responsibility :,
+//   payment,
+//   etc,
+// } : {
+//   default : [],
+//   personalinfo: [],
+//   safety: [],
+//   responsibility: [],
+//   payment: [],
+//   etc: [],
+// })(selectedBlocks: string) =>
+//   api.post("/template/make", selectedBlocks);
+export const registerTemplate = ({
+  defaultBlock,
+  personalinfoBlock,
+  safetyBlock,
+  responsibilityBlock,
+  paymentBlock,
+  etcBlock,
+}: {
+  defaultBlock: any[];
+  personalinfoBlock: any[];
+  safetyBlock: any[];
+  responsibilityBlock: any[];
+  paymentBlock: any[];
+  etcBlock: any[];
+}) => {
+  // API 호출을 수행하고 결과를 반환합니다.
+  return api.post("/template/make", {
+    defaultBlock: defaultBlock,
+    personalinfoBlock: personalinfoBlock,
+    safetyBlock: safetyBlock,
+    responsibilityBlock: responsibilityBlock,
+    paymentBlock: paymentBlock,
+    etcBlock: etcBlock,
+  });
+};
 
 export const registerForm = (data: FormData, templateId: number) =>
   api.post(`/form/make/${templateId}`, data);
